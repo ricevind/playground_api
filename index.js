9b0ee3adf44ed17350ddce5ee1f86381be662113
ricevind <ricevind@gmail.com>
2018-07-08 20:10:28 +0200

    port from process.env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(PORT, () => console.log('Example app listening on port 3000!'));
